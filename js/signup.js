0712fbc8d97345e853db10a74b7fc3d59ab38443
khaled279 <khaled.sabed30@yahoo.com>
2022-06-03 05:51:03 +0200

    auto Email confirmation
    Skips email confirmation

--- js/signup.js
+++ js/signup.js
@@ -1,8 +1,10 @@
 console.log("linked");
 const signupButton = document.getElementById("signup"); 
-const url = "http://localhost:4000/users/signup" ; 
-const loginUrl = "http://localhost:4000/users/login"
+const url = "users/signup" ; 
+const loginUrl = "users/login"
+const baseUrl = "http://localhost:4000/"
 let res="" ; 
+let confirmationRes = "" ; 
 const inputName = document.getElementById("name"); 
 const inputPassword = document.getElementById("password");
 const inputEmail = document.getElementById("email"); 
@@ -13,7 +15,7 @@ signupButton.addEventListener('click',async (event)=>{
             "email": inputEmail.value ,
             "password": inputPassword.value
         }
-        res = await fetch(loginUrl , {
+        res = await fetch(baseUrl+loginUrl , {
             method: 'POST', 
             credentials: 'same-origin', 
             headers: {
@@ -33,7 +35,7 @@ signupButton.addEventListener('click',async (event)=>{
          "email": inputEmail.value ,
         "password": inputPassword.value
     }
-    res = await fetch(url , {
+    res = await fetch(baseUrl +url , {
         method: 'POST', 
         credentials: 'same-origin', 
         headers: {
@@ -43,8 +45,21 @@ signupButton.addEventListener('click',async (event)=>{
         },
         body : JSON.stringify(postData)
     }
-    )
-    console.log("request output" , `${res.json()}`)
+    ).then(async (res)=>{
+        jsonRes = await res.json(); 
+        confirmationRes = await fetch(baseUrl+"confirm" ,{
+            method: 'POST', 
+            credentials: 'same-origin', 
+            headers: {
+                'Content-type' : 'application/json',
+                'Accept' : '*/*', 
+                'Connection': 'keep-alive'
+            },
+            body : JSON.stringify({"confirmationCode": jsonRes.user.confirmationCode})
+        })
+    })
+        console.log(await confirmationRes.json())    
+
 }
 }) ; 
 
